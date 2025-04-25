c1f383a59013477072d10bf9d61726000b4f8ee0
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2025-04-25 17:14:22 -0300

    inicio de projeto

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="bg-slate-950 min-h-screen text-slate-200 flex justify-center">
+      <div className="container flex justify-between py-6 text-xl font-semibold">
+        <h3 className="text-gradient-purple">
+          /TjFaccipieri
+        </h3>
+        <ul className="flex gap-4">
+          <li className="text-gradient-purple">Home</li>
+          <li className="hover:underline">Sobre</li>
+          <li className="hover:text-gradient-purple">Projetos</li>
+          <li className="hover:text-gradient-purple">Skills</li>
+          <li className="hover:text-gradient-purple">Experiência</li>
+          <li className="hover:text-gradient-purple">Contato</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
